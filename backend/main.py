cebcdc1ffa889d87b47498a3dcf8279111781697
Ethan Carter Edwards <ethan@ethancedwards.com>
2026-03-28 22:55:49 -0400

    fix states

--- backend/main.py
+++ backend/main.py
@@ -5,7 +5,7 @@ if __name__ == "__main__":
     subprocess.run([
         "gunicorn", "app.main:app",
         "--bind", "0.0.0.0:8000",
-        "--workers", "4",
-        "--threads", "4",
+        # "--workers", "4",
+        # "--threads", "4",
         "--worker-class", "gthread",
     ])
